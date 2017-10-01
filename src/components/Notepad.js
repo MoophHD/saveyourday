e2cfe5493f964048d89f42265d06c8424f894e4b
MoophHD <d_sizykh@mail.ru>
2017-10-01 10:17:47 +0300

    ctrl+arrows date change & stuff

--- src/components/Notepad.js
+++ src/components/Notepad.js
@@ -1,22 +1,26 @@
+/* eslint-disable */
+
 import React, { Component } from 'react'
 import setSelectionRange from '../gist/setSelectionRange'
 
 class Notepad extends Component {
     constructor(props) {
         super(props);
-        this.handleTabPress = this.handleTabPress.bind(this);
+
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
-    /* eslint-disable */
     componentDidMount() {
         this.area.value = this.props.cookiesValue;
         this.area.spellcheck = false;
+        this.localRegFormatted = /\d{1,2}\-\d{1,2}(\ \:\ )\d{1,2}\-\d{1,2}/;
+        this.regFormatted = /\d{1,2}\-\d{1,2}(\ \:\ )\d{1,2}\-\d{1,2}/g;
 
-        this.area.addEventListener('keydown', (e) => this.handleTabPress(e));
+        this.area.addEventListener('keydown', (e) => this.handleKeyPress(e));
     }
 
     checkAndFixZeros(slice) {
-        return slice.toString().length == 1 ? '0' + slice : slice;
+        return slice < 10 ? '0' + slice : slice.toString();
     }
 
     parseDate(date) {
@@ -25,13 +29,12 @@ class Notepad extends Component {
         date.forEach(function(el, ind) {
             if (/\d{1,2}\.\d{1,2}/.test(el)) {
                 el = el.split('.');
-                
                 el[0] = this.checkAndFixZeros(el[0]);
-
-                el[1] = (parseInt(el[1])*6).toString();
+                
+                if (el[1].length == 1) el[1] = (parseInt(el[1])*6).toString();
                 
                 el[1] = this.checkAndFixZeros(el[1]);
-
+                
                 el = el.join('-');
 
                 date[ind] = el;
@@ -42,16 +45,14 @@ class Notepad extends Component {
         return date.join(' : ');
     }
 
-
-
-    handleTabPress(e) {
-        if (e.key == 'Tab') {
+    handleKeyPress(e) {
+        if (e.keyCode == 9) {
             e.preventDefault();
             
             let cursorPos = this.area.selectionStart;            
             let areaValue = this.area.value;
             let unformattedArr = areaValue.match(/(\d{1,4}|\d{1,2}\.\d{1,2})(\:|\ )(\d{1,2}\.\d{1,2}|\d{1,4})/g);
-            let regFormatted = /\d{1,2}\-\d{1,2}(\ \:\ )\d{1,2}\-\d{1,2}/g;
+            let regFormatted = this.regFormatted;
             let formattedArr;
             let lastFormatted;
             let lastInd;
@@ -60,9 +61,11 @@ class Notepad extends Component {
                 unformattedArr.forEach(function(slice, ind) {
                     areaValue = areaValue.replace(slice, this.parseDate(slice));    
                 }, this);
-            }
+            } 
             
             let formattedChuncks = areaValue.match(/\d{1,2}\-\d{1,2}(\ \:\ )\d{1,2}\-\d{1,2}/g);
+
+            if (!unformattedArr && !formattedChuncks) return;
 
             if (areaValue.search(regFormatted) != -1) {
                 let prevResult = areaValue.match(regFormatted)[0];
@@ -77,10 +80,10 @@ class Notepad extends Component {
                     lastInd = formattedArr.index + prevResult.length;
                   }
 
-                areaValue = areaValue.slice(0, lastInd) + '\n' + this.addMins(lastFormatted ? lastFormatted : formattedChuncks[formattedChuncks.length - 1], 30) + areaValue.slice(lastInd, areaValue.length) ;
+                areaValue = areaValue.slice(0, lastInd) + '\n' + this.addMins(lastFormatted ? lastFormatted : formattedChuncks[formattedChuncks.length - 1], 30, true) + areaValue.slice(lastInd, areaValue.length) ;
             }
-            console.log(lastFormatted);
             this.area.value = areaValue;
+
             if (lastFormatted == areaValue.match(regFormatted)[0]) {
                 this.area.value += '\n'
                 setSelectionRange(this.area, areaValue.length+1, areaValue.length+1);
@@ -90,38 +93,93 @@ class Notepad extends Component {
                 this.area.value += '\n'
                 setSelectionRange(this.area, lastInd, lastInd);
             }
+        } else if (e.ctrlKey && ( e.key == "ArrowDown" || e.key == "ArrowUp" )) {
+            e.preventDefault()
+            console.log(e);
+            console.log(this.regFormatted.test(selection));
+            let selection = window.getSelection().toString();
+            if (this.regFormatted.test(selection)) {
+                let area = this.area.value;
+                let realInd = area.indexOf(selection) + selection.search(this.localRegFormatted);
+                let selectedDate = this.localRegFormatted.exec(selection)[0];
+
+                let mod = e.key == "ArrowUp" ? 1 : -1;
+
+                area = area.slice(0, realInd) + this.addMins(selectedDate, 15*mod, false) + area.slice(realInd + selectedDate.length, area.length);
+                this.area.value = area;
+
+                setSelectionRange(this.area, realInd, realInd + selectedDate.length);
+            }
         }
     }
 
-
-
-    addMins(date, minValue) {
+    addMins(date, minValue, toLast=true) {
         let hrs = (minValue > 59) ? ~~(minValue/60) : 0;
         let mins = minValue % 60 ? minValue % 60 : 0;
         let dateChuncks = date.split(' : ');
         let tempChunck;
+
         
-        dateChuncks.forEach((el, ind) => {
-            tempChunck = el.split('-');
-            tempChunck[0] = parseInt(tempChunck[0]) + hrs;
-            tempChunck[1] = parseInt(tempChunck[1]) + mins;
-            if (tempChunck[0] > 12) {
-                tempChunck[0] -= 12;
-            }
+        switch (toLast) {
+            case true:
+                    dateChuncks[0] = dateChuncks[1];
+            
+                    tempChunck = dateChuncks[1].split('-');
+            
+                    tempChunck[0] = parseInt(tempChunck[0]) + hrs;
+                    tempChunck[1] = parseInt(tempChunck[1]) + mins;
+            
+                    if (tempChunck[0] > 11) {
+                        tempChunck[0] -= 12;
+                    }
+            
+                    if (tempChunck[1] > 59) {
+                        tempChunck[0] += ~~(tempChunck[1]/60);
+            
+                        tempChunck[1] = tempChunck[1] % 60;
+                    }
+                    tempChunck[0] = this.checkAndFixZeros(tempChunck[0]);
+                    tempChunck[1] = this.checkAndFixZeros(tempChunck[1]);
+            
+                    dateChuncks[1] = tempChunck.join('-');
+                    return dateChuncks.join(' : ');
+                    
+            case false:
+                    dateChuncks.forEach((chunck,ind ) => {
+                        tempChunck = chunck.split('-');
 
-            if (tempChunck[1] > 59) {
-                tempChunck[0] += ~~(tempChunck[1]/60);
+                        tempChunck[0] = parseInt(tempChunck[0]) + hrs;
+                        tempChunck[1] = parseInt(tempChunck[1]) + mins;
 
-                tempChunck[1] = tempChunck[1] % 60;
-            }
-            tempChunck[0] = this.checkAndFixZeros(tempChunck[0]);
-            tempChunck[1] = this.checkAndFixZeros(tempChunck[1]);
 
-            dateChuncks[ind] = tempChunck.join('-');
-        }, this)
+                        console.log(tempChunck);
+                        if (tempChunck[1] < 0 ) {
+                            tempChunck[0] -= ~~(tempChunck[1]/60) + 1;
+                            tempChunck[1] += 60;
+                        } else if (tempChunck[1] > 59) {
+                            tempChunck[0] += ~~(tempChunck[1]/60);
+                            tempChunck[1] -= 60;
+                        }
 
-        return dateChuncks.join(' : ');
-    }
+                        if (tempChunck[0] < 0 ) {
+                            tempChunck[0] += 12;
+                        } else if (tempChunck[0] > 11) {
+                            tempChunck[0] -= 12;
+                        }
+
+                        tempChunck[0] = this.checkAndFixZeros(tempChunck[0]);
+                        tempChunck[1] = this.checkAndFixZeros(tempChunck[1]);
+
+                        dateChuncks[ind] = tempChunck.join('-');
+
+                    })
+                    return dateChuncks.join(' : ');
+                }
+
+
+            
+        }
+
     
     render() {
         return (
